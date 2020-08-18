281685d68dd9e02a69556644899417b3532adde7
NIC <728075666@qq.com>
2020-08-18 21:47:06 +0800

    somehow

---
+++ src/pages/home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <nav-header></nav-header>
+        <router-view></router-view>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+
+<script>
+import NavHeader from
+import NavFooter from
+export default {
+    name: "home"
+}
+</script>
+
+<style scoped>
+
+</style>
